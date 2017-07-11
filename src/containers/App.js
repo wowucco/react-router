0a199e364cfebd15bdd849bff8fd2f39b903df4a
Vladimir Scherbak <vscherbak@newagesol.com>
2017-07-11 18:35:48 +0300

    react-router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,37 +1,16 @@
 import React, { Component } from 'react'
-import Home from '../components/Home'
-import Admin from '../components/Admin'
-import Genre from '../components/Genre'
+import {Link} from 'react-router'
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            route: window.location.hash.substr(1)
-        }
-    }
-    componentDidMount() {
-        window.addEventListener('hashchange', () => {
-            this.setState({
-                route: window.location.hash.substr(1)
-            })
-        })
-    }
     render() {
-        let Child
-        switch (this.state.route) {
-            case '/admin': Child = Admin; break;
-            case '/genre': Child = Genre; break;
-            default: Child = Home;
-        }
         return (
             <div className='container'>
                 <h2>App</h2>
                 <ul>
-                    <li><a href='#/admin'>Admin</a></li>
-                    <li><a href='#/genre'>Genre</a></li>
+                    <li><Link to='/admin'>Admin</Link></li>
+                    <li><Link to='/genre'>Genre</Link></li>
                 </ul>
-                <Child />
+                {this.props.children}
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import App from './containers/App'
-
+import Home from './components/Home'
+import Admin from './components/Admin'
+import Genre from './components/Genre'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 
 render(
-    <App />,
+    <Router histiry={browserHistory}>
+        <Route path='/' component={App}>
+            <IndexRoute component={Home} />
+            <Route path='admin' component={Admin} />
+            <Route path='genre' component={Genre} />
+        </Route>
+    </Router>,
     document.getElementById('root')
 )
