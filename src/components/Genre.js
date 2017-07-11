fef65acc8799e4b120c7c76fcd4604795bfe2701
Vladimir Scherbak <vscherbak@newagesol.com>
2017-07-11 17:55:35 +0300

    simple router

---
+++ src/components/Genre.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Genre extends Component {
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-md-12'>Page /genre</div>
+            </div>
+        )
+    }
+}
