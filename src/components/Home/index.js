b2de0d0600baba899e0c2cf5de86fe948d557afc
Vladimir Scherbak <vscherbak@newagesol.com>
2017-07-12 15:17:35 +0300

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,28 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 
 export default class Home extends Component {
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault()
+        const value = e.target.elements[0].value.toLowerCase()
+        this.context.router.push(`/genre/${value}`)
+    }
     render() {
         return (
             <div className='row'>
-                <div className='col-md-12'>Page /</div>
+                <div className='col-md-12'>Home</div>
+                <form className='col-md-4' onSubmit={this.handleSubmit}>
+                    <input type='text' placeholder='genreName'/>
+                    <button	type='submit'>GoTo</button>
+                </form>
             </div>
         )
     }
+}
+
+Home.contextTypes	=	{
+    router:	PropTypes.object.isRequired
 }
